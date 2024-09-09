8bb832bcb303f3a18ac93fa90b192fa3e6e4682a
sudhanshusingh-g <sudsingh32@gmail.com>
2024-09-09 12:18:03 +0530

    Implementd all functionalities

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const cartQuantity=document.querySelector(".cart span");
 const addToCart = document.querySelector(".add_to_cart");
 const cartEmpty = document.querySelector(".cart_empty");
 const cartProduct = document.querySelector(".product_in_cart");
+const deleteProduct = document.querySelector(".remove_product");
+
 
 // ProductImages
 const productImages = [
@@ -67,7 +69,17 @@ decrement.addEventListener("click", () => {
 });
 
 addToCart.addEventListener("click",()=>{
-    cartQuantity.textContent=counter;
+    if (counter > 0) {
+      cartEmpty.style.display = "none";
+      cartProduct.classList.add("active");
+      cartQuantity.textContent = counter;
+      document.querySelector(
+        ".price_calculation p:nth-child(1)"
+      ).textContent = `$125.00 x ${counter}`;
+      document.querySelector(
+        ".price_calculation p:nth-child(2)"
+      ).textContent = `$${125 * counter}.00`;
+    }
 });
 
 // Show and hide cart popup
@@ -77,4 +89,23 @@ cartIcon.addEventListener("click", () => {
     cartEmpty.style.display="none";
     cartProduct.classList.add("active");
   }
+});
+
+const thumbnails = document.querySelectorAll(".product_thumbnail_image img");
+thumbnails.forEach((thumbnail, index) => {
+  thumbnail.addEventListener("click", () => {
+    currentIndex = index;
+    productImage.src = productImages[currentIndex];
+    thumbnails.forEach((thumb) => thumb.classList.remove("active"));
+    thumbnail.classList.add("active");
+  });
+});
+
+deleteProduct.addEventListener("click",()=>{
+    cartEmpty.style.display="block";
+    cartProduct.classList.remove("active");
+    quantity.textContent=0;
+    counter=0;
+    cartQuantity.textContent=0;
+    cartPopup.classList.remove("active");
 });
